aaae72d582a913b2976e60544b858dc244e29fbf
applejuiceroman <roman_yablochkin@mail.ru>
2026-05-19 10:25:18 +0300

    lesson_3 commit

--- src/main/java/lesson_3/Park.java
+++ src/main/java/lesson_3/Park.java
@@ -16,15 +16,4 @@ public class Park {
             System.out.println(name + ". Часы работы: " + workHours + ". Цена: " + price + " BYN");
         }
     }
-
-    public static void main(String[] args) {
-        Park park = new Park();
-        Attraction kidsMiniTrain = park.new Attraction("Детский мини-поезд", "10:00-20:00", 2.5);
-        Attraction ferrisWheel = park.new Attraction("Колесо обозрения", "11:00-19:00", 4.2);
-        Attraction labyrinth = park.new Attraction("Лабиринт", "11:00-19:00", 7);
-
-        kidsMiniTrain.displayInfo();
-        ferrisWheel.displayInfo();
-        labyrinth.displayInfo();
-    }
 }
